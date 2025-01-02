3471be6cd2dabc1aa0e17de8be9fddae7aa8df07
sunidhisingh66541 <sunidhikr2666@gmail.com>
2025-01-02 23:51:11 +0530

    this is 2nd code

--- basiccode.py
+++ basiccode.py
@@ -1,2 +1,2 @@
 print('hey! I am sunidhi' )
-print('the very first time i am learning from gaurav i do not like him even')
+print('the very first time i am learning from gaurav i do not like him even, he just shouted on me i hate him')
